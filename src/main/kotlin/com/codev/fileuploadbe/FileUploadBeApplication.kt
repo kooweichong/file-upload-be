da174e500db56769b3a5ce0457ddc2af396314d5
weichong <kooweichong@ecquaria.com>
2023-02-15 12:04:39 +0800

    Initial commit

---
+++ src/main/kotlin/com/codev/fileuploadbe/FileUploadBeApplication.kt
@@ -0,0 +1,11 @@
+package com.codev.fileuploadbe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FileUploadBeApplication
+
+fun main(args: Array<String>) {
+	runApplication<FileUploadBeApplication>(*args)
+}
