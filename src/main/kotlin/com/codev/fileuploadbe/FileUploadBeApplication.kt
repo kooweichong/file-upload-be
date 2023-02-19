c652e48ed4ca58b1dc57934eaffb976675527da6
weichong <kooweichong@ecquaria.com>
2023-02-19 22:35:26 +0800

    Set up Configuration to allow large file upload

--- src/main/kotlin/com/codev/fileuploadbe/FileUploadBeApplication.kt
+++ src/main/kotlin/com/codev/fileuploadbe/FileUploadBeApplication.kt
@@ -1,11 +1,38 @@
 package com.codev.fileuploadbe
 
+import io.r2dbc.spi.ConnectionFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+import org.springframework.http.codec.ServerCodecConfigurer
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator
+import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.web.reactive.config.WebFluxConfigurer
 
+
+@Configuration
+@EnableWebFlux
 @SpringBootApplication
-class FileUploadBeApplication
+class FileUploadBeApplication : WebFluxConfigurer {
+    override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
+        configurer.defaultCodecs().maxInMemorySize(-1)
+    }
+
+    @Bean
+    fun initializer(connectionFactory: ConnectionFactory): ConnectionFactoryInitializer {
+        val initializer: ConnectionFactoryInitializer = ConnectionFactoryInitializer()
+        initializer.setConnectionFactory(connectionFactory)
+        initializer.setDatabasePopulator(ResourceDatabasePopulator(ClassPathResource("schema.sql")))
+
+        return initializer
+    }
+
+}
 
 fun main(args: Array<String>) {
-	runApplication<FileUploadBeApplication>(*args)
+    runApplication<FileUploadBeApplication>(*args)
 }
